bc6f3e9b428832f6c83c583b09b47dd2bbb18e60
Nikolaus Trixner <n.trixner@gmx.at>
2019-04-24 16:50:28 +0200

    Cleanup code

--- server/src/main/java/at/ntrixner/base/api/BaseController.java
+++ server/src/main/java/at/ntrixner/base/api/BaseController.java
@@ -1,12 +1,9 @@
 package at.ntrixner.base.api;
 
 import at.ntrixner.base.dto.MessageDTO;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.mail.Message;
 
 @RestController
 @RequestMapping("/base")
