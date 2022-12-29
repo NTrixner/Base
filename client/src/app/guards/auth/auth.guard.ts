4272413eb2a2752bb60d3f0802f829c0ce63fa63
ntrixner <n.trixner@gmx.at>
2022-12-29 21:37:01 +0100

    #92 Correctly forward parameters to frontend
    Also save user in local storage

--- client/src/app/guards/auth/auth.guard.ts
+++ client/src/app/guards/auth/auth.guard.ts
@@ -1,19 +1,14 @@
-import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from '../../services/auth/auth.service';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree,} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../../services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) {
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -23,17 +18,16 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (this.authService.user) {
+    if (!!this.authService.user) {
       return true;
     } else {
-      const userToken = window.sessionStorage.getItem('userToken');
-      console.log('authGuard: ' + userToken);
+      const userToken = localStorage.getItem('userToken');
       if (userToken != null) {
         this.authService.setTokenAndLoadUserData(userToken);
         return true;
       } else {
         this.router.navigate(['/login'], {
-          queryParams: { returnUrl: state.url },
+          queryParams: {returnUrl: state.url},
         });
         return false;
       }
